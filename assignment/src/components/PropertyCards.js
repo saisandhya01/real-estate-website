7262e8b2e04e9c6efa44bf3e2045c735df5eb909
Sai <saisandhya187@gmail.com>
2022-10-30 10:20:12 +0530

    Add display message for 0 results

--- assignment/src/components/PropertyCards.js
+++ assignment/src/components/PropertyCards.js
@@ -14,7 +14,37 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-function PropertyCards(props) {
+function PropertyCards({ filters }) {
+  const renderData = (filters) => {
+    const filteredProperties = Properties.filter(
+      (el) =>
+        ((el.location !== filters.location && filters.location === "") ||
+          el.location === filters.location) &&
+        ((el.property !== filters.propertyType &&
+          filters.propertyType === "") ||
+          el.property === filters.propertyType) &&
+        el.price >= filters.minPrice &&
+        el.price <= filters.maxPrice &&
+        el.squarefeet >= filters.minArea &&
+        el.squarefeet <= filters.maxArea
+    );
+
+    if (filteredProperties.length === 0) {
+      return (
+        <div className="no-filters">
+          <h1>No properties found for the above filters.</h1>
+        </div>
+      );
+    }
+    return filteredProperties.map((filteredProperty) => (
+      <Grid item xs={2} sm={4} md={4} key={filteredProperty.id}>
+        <SinglePropertyCard
+          key={filteredProperty.id}
+          details={filteredProperty}
+        />
+      </Grid>
+    ));
+  };
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -23,18 +53,18 @@ function PropertyCards(props) {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {Properties.filter(
+          {renderData(filters)}
+          {/*Properties.filter(
             (el) =>
-              ((el.location !== props.filters.location &&
-                props.filters.location === "") ||
-                el.location === props.filters.location) &&
-              ((el.property !== props.filters.propertyType &&
-                props.filters.propertyType === "") ||
-                el.property === props.filters.propertyType) &&
-              el.price >= props.filters.minPrice &&
-              el.price <= props.filters.maxPrice &&
-              el.squarefeet >= props.filters.minArea &&
-              el.squarefeet <= props.filters.maxArea
+              ((el.location !== filters.location && filters.location === "") ||
+                el.location === filters.location) &&
+              ((el.property !== filters.propertyType &&
+                filters.propertyType === "") ||
+                el.property === filters.propertyType) &&
+              el.price >= filters.minPrice &&
+              el.price <= filters.maxPrice &&
+              el.squarefeet >= filters.minArea &&
+              el.squarefeet <= filters.maxArea
           ).map((filteredProperty) => (
             <Grid item xs={2} sm={4} md={4} key={filteredProperty.id}>
               <SinglePropertyCard
@@ -42,7 +72,7 @@ function PropertyCards(props) {
                 details={filteredProperty}
               />
             </Grid>
-          ))}
+          ))*/}
         </Grid>
       </Box>
     </div>
--- assignment/src/components/SinglePropertyCard.js
+++ assignment/src/components/SinglePropertyCard.js
@@ -6,35 +6,37 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-export default function SinglePropertyCard(props) {
+export default function SinglePropertyCard({ details }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={require("../assets/sample_image.jpg")}
-        alt="sample property"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.details.name}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div">
-          {props.details.location}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.details.address}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Price : {`$${props.details.price}k`}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Squarefeet : {`${props.details.squarefeet}m3`}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Property type : {props.details.property}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className="carddiv">
+      <Card sx={{ maxWidth: 400 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require("../assets/sample_image.jpg")}
+          alt="sample property"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {details.name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {details.location}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {details.address}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Price : {`$${details.price}k`}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Squarefeet : {`${details.squarefeet}m3`}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Property type : {details.property}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
